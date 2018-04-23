6ebef0eeb8e954d15e3c55277d600d9486ad4b4a
louis delbosc <louisdelbosc@gmail.com>
2018-04-23 12:24:44 +0200

    Generate button from data

--- src/calculator/buttongrid.jsx
+++ src/calculator/buttongrid.jsx
@@ -1,42 +1,59 @@
 import React, { Component } from 'react';
 
+const handleClick = (v) => { console.log(v.target.value) };
+
 export class ButtonGrid extends Component {
+
+  renderButtons() {
+    const buttonData = [
+      { label: '1', onClick: handleClick },
+      { label: '2', onClick: handleClick },
+      { label: '3', onClick: handleClick },
+      { label: '4', onClick: handleClick },
+      { label: '5', onClick: handleClick },
+      { label: '6', onClick: handleClick },
+      { label: '7', onClick: handleClick },
+      { label: '8', onClick: handleClick },
+      { label: '9', onClick: handleClick },
+      { label: '0', onClick: handleClick },
+      { label: '.', onClick: handleClick },
+      { label: '=', onClick: handleClick },
+    ];
+    return buttonData.map(({label, onClick}) => (
+      <button key={label} className="btn btn-light col-4" onClick={onClick} value={label}>
+        {label}
+      </button>
+    ));
+  }
+
+  renderOps() {
+    const buttonData = [
+      { label: '-', onClick: handleClick },
+      { label: '*', onClick: handleClick },
+      { label: '-', onClick: handleClick },
+      { label: '+', onClick: handleClick },
+      { label: 'C', onClick: handleClick },
+    ];
+    return buttonData.map(({label, onClick}) => (
+      <div className="row">
+        <button key={label} className="btn btn-light col-12" onClick={onClick} value={label}>
+          {label}
+        </button>
+      </div>
+    ));
+  }
+
   render() {
     return (
       <div className="container" >
         <div className="row">
           <div className="col-9">
             <div className="row">
-              <button className="btn btn-light col-4">1</button>
-              <button className="btn btn-light col-4">2</button>
-              <button className="btn btn-light col-4">3</button>
-              <button className="btn btn-light col-4">4</button>
-              <button className="btn btn-light col-4">5</button>
-              <button className="btn btn-light col-4">6</button>
-              <button className="btn btn-light col-4">7</button>
-              <button className="btn btn-light col-4">8</button>
-              <button className="btn btn-light col-4">9</button>
-              <button className="btn btn-light col-4">0</button>
-              <button className="btn btn-light col-4">.</button>
-              <button className="btn btn-light col-4">=</button>
+              {this.renderButtons()}
             </div>
           </div>
           <div className="col-3">
-            <div className="row">
-              <button className="btn btn-light">/</button>
-            </div>
-            <div className="row">
-              <button className="btn btn-light">*</button>
-            </div>
-            <div className="row">
-              <button className="btn btn-light">-</button>
-            </div>
-            <div className="row">
-              <button className="btn btn-light">+</button>
-            </div>
-            <div className="row">
-              <button className="btn btn-light">C</button>
-            </div>
+            {this.renderOps()}
           </div>
         </div>
       </div>
